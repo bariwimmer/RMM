a986e39009aa4275f76156c206c4ada9e710c0ec
baribowman <baribowman@gmail.com>
2020-11-06 15:47:47 -1000

    Connecting 2 Pages

--- app/src/main/java/com/example/mysqldemotrial/MainActivity.java
+++ app/src/main/java/com/example/mysqldemotrial/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
         String type = "login";
         BackgroundWorker backgroundWorker = new BackgroundWorker(this);
         backgroundWorker.execute(type, username, password);
+
+
+
+     Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 
     public void OpenReg(View view) {
---
+++ app/src/main/java/com/example/mysqldemotrial/MenuActivity.java
@@ -0,0 +1,20 @@
+package com.example.mysqldemotrial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmenu);
+
+    }
+
+}
